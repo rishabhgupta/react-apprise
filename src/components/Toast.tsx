edf71003f846fc1be3bdc053a048549cde1be15f
rishabhgupta <rishabhgupta@msn.com>
2020-05-14 01:17:22 +0530

    build(nvm): adds nvmrc file with 12.13.1 version

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useState, useEffect } from "react";
-import './Toast.css';
+import "./Toast.css";
 
 type toast = {
     id: number;
@@ -7,16 +7,15 @@ type toast = {
     description: string;
     backgroundColor: string;
     icon: string;
-}
+};
 interface IProps {
     /** represents an array that will contain objects */
     toastList: toast[];
     /** placement of the notification container on the page. */
-    position?: 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left';
+    position?: "top-right" | "bottom-right" | "top-left" | "bottom-left";
 }
 
 const Toast = (props: IProps): ReactElement => {
-
     const { toastList, position } = props;
 
     const [list, setList] = useState(toastList);
@@ -28,35 +27,31 @@ const Toast = (props: IProps): ReactElement => {
     return (
         <>
             <div className={`notification-container ${position}`}>
-                {
-                    list.map((toast, i: number) =>     
-                        <div 
-                            key={i}
-                            className={`notification toast ${position}`}
-                            style={{ backgroundColor: toast.backgroundColor }}
-                        >
-                            <button>
-                                X
-                            </button>
-                            <div className="notification-image">
-                                <img src={toast.icon} alt="" />
-                            </div>
-                            <div>
-                                <p className="notification-title">{toast.title}</p>
-                                <p className="notification-message">
-                                    {toast.description}
-                                </p>
-                            </div>
+                {list.map((toast, i: number) => (
+                    <div
+                        key={i}
+                        className={`notification toast ${position}`}
+                        style={{ backgroundColor: toast.backgroundColor }}
+                    >
+                        <button>X</button>
+                        <div className="notification-image">
+                            <img src={toast.icon} alt="" />
                         </div>
-                    )
-                }
+                        <div>
+                            <p className="notification-title">{toast.title}</p>
+                            <p className="notification-message">
+                                {toast.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     );
-}
+};
 
 Toast.defaultProps = {
-    position: 'bottom-right'
-}
+    position: "bottom-right"
+};
 
 export default Toast;
