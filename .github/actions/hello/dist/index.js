bf55d67784eaee731d7d728f0ef951bfed02cd1b
rishabhgupta <rishabhgupta@msn.com>
2020-05-17 22:17:35 +0530

    ci: fix

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -3716,6 +3716,7 @@ try {
     const stringValue = core.getInput('string');
     const splitBy = core.getInput('split-by');
     const value = stringValue.split(splitBy);
+    console.log(value);
     core.setOutput("value", value);
 } catch (err) {
     core.setFailed(err.message);
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -5,6 +5,7 @@ try {
     const stringValue = core.getInput('string');
     const splitBy = core.getInput('split-by');
     const value = stringValue.split(splitBy);
+    console.log(value);
     core.setOutput("value", value);
 } catch (err) {
     core.setFailed(err.message);
