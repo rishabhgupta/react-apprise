fa88fcfe962494754c48c2c681395fbcb8ece277
rishabhgupta <rishabhgupta@msn.com>
2020-05-18 08:51:54 +0530

    ci: adds spitby from github

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,11 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
     const stringValue = core.getInput('string');
     const splitBy = core.getInput('split-by');
     const value = stringValue.split(splitBy);
-    console.log(value);
     core.setOutput("value", value);
 } catch (err) {
     core.setFailed(err.message);
