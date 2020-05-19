4fa6fde2f9f62b867c7f5ab7b88598a6b5999429
rishabhgupta <rishabhgupta@msn.com>
2020-05-20 02:52:32 +0530

    ci: adds issue task

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const github = require('@actions/githu');
+
+try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
+
+    const octokit = new github.Github(token);
+
+    const response = octokit.issues.create({
+        ...github.context.repo,
+        title,
+        body,
+        assignees: assignees ? assignees.split(',') : undefined
+    })
+
+    core.setOutput('issue', JSON.stringify(response.data));
+
+} catch (err) {
+    core.setFailed(error.message)
+}
