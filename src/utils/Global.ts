8153f08bfedcae66a21ff6232a8c764e061e2fbf
rishabhgupta <rishabhgupta@msn.com>
2020-05-24 23:40:37 +0530

    feat: adds global styles

---
+++ src/utils/Global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+import { PRIMARY_FONT } from './typography';
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        color: ${props => 'black'};
+        font-size: 16px;
+        box-sizing: border-box;
+    }
+
+    *. , *:before, *:after {
+        box-sizing: border-box;
+    }
+
+    body {
+        margin: 0;
+        font-family: ${PRIMARY_FONT}
+    }
+`
