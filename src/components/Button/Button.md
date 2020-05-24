3e4be50fe56a874b43b3465ff584962091176438
rishabhgupta <rishabhgupta@msn.com>
2020-05-24 23:39:46 +0530

    feat(buttons): adds buttons components

--- src/components/Button/Button.md
+++ src/components/Button/Button.md
@@ -1,6 +1,44 @@
-
+### Button Types
 ```jsx
-import PrimaryButton, {InfoButton, WarningButton, DangerButton} from './Button';
+import { Button }from './Button';
+import { ThemeProvider } from 'styled-components';
+import {defaultTheme, darkTheme} from '../../utils/themes';
+class ButtonWrapper extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            useDarkTheme: false,
+        }
+        this.setDarkTheme = this.setDarkTheme.bind(this);
+    }
+
+    setDarkTheme() {
+        const { useDarkTheme}  = this.state;
+        this.setState({
+            'useDarkTheme': !useDarkTheme,
+        })
+    }
+
+    render() {
+        return (
+            <ThemeProvider theme={this.state.useDarkTheme ? darkTheme : defaultTheme}>
+                <button style={{margin: "24px 0", padding: "8px", background: 'none'}} onClick={this.setDarkTheme}>Toggle Dark Theme</button>
+                <br/>
+                <Button primary>Primary</Button>
+                <span> </span>
+                <Button secondary>Secondary</Button>
+            </ThemeProvider>
+        )
+    }
+}
+<ButtonWrapper/>
+```
+
+### Disabled State
+```jsx
+import { Button } from './Button';
+import { ThemeProvider } from 'styled-components';
+import {defaultTheme} from '../../utils/themes';
 class ButtonWrapper extends React.Component {
     constructor() {
         super();
@@ -8,19 +46,68 @@ class ButtonWrapper extends React.Component {
 
     render() {
         return (
-            <div>
-                <PrimaryButton>Primary</PrimaryButton>
+             <ThemeProvider theme={defaultTheme}>
+                <Button primary disabled>Primary</Button>
                 <span> </span>
-                <InfoButton>Info</InfoButton>
-                <span> </span>
-                <WarningButton>Warn</WarningButton>
-                <span> </span>
-                <DangerButton>Danger</DangerButton>
-            </div>
+                <Button secondary disabled>Secondary</Button>
+            </ThemeProvider>
         )
     }
 }
 <ButtonWrapper/>
+```
+---
+
+## Modifiers
+
+### Size Modifiers
+```jsx
+import { Button } from './Button';
+import { ThemeProvider } from 'styled-components';
+import {defaultTheme} from '../../utils/themes';
+class ButtonWrapper extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <ThemeProvider theme={defaultTheme}>
+                <Button modifiers="small" >Primary</Button>
+                <span> </span>
+                <Button>Primary</Button>
+                 <span> </span>
+                <Button  modifiers="large">Primary</Button>
+            </ThemeProvider>
+        )
+    }
+}
+<ButtonWrapper/>
+```
 
 
+### Status Modifiers
+```jsx
+import { Button } from './Button';
+import { ThemeProvider } from 'styled-components';
+import {defaultTheme} from '../../utils/themes';
+class ButtonWrapper extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <ThemeProvider theme={defaultTheme}>
+                <Button>Primary</Button>
+                <span> </span>
+                <Button modifiers={["warning"]}>Warning</Button>
+                <span> </span>
+                <Button modifiers={["danger"]}>Danger</Button>
+
+            </ThemeProvider>
+        )
+    }
+}
+<ButtonWrapper/>
 ```
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,36 +1,111 @@
-import styled from "styled-components";
+import styled, { StyledFunction } from "styled-components";
 import { defaultTheme, typeScale } from "../../utils";
+import { applyStyleModifiers } from 'styled-components-modifiers';
+import { ReactNode, ButtonHTMLAttributes, FunctionComponent } from "react";
+import { thisExpression } from "@babel/types";
 
-const Button = styled.button`
-    padding: 12px 24px;
+const HEADING_MODIFIERS = {
+    small: () => `
+        font-size: ${typeScale.helperText};
+        padding: 8px;
+    `,
+    large: () => `
+        font-size: ${typeScale.header5};
+        padding: 16px 24px;
+    `,
+    warning: () => `
+        background-color: ${defaultTheme.status.warningColor};
+        color: ${defaultTheme.textColorInverted};
+        &:hover, &:focus {
+            background-color: ${defaultTheme.status.warningColorHover};
+            outline: none;
+        }
+        &:active {
+            background-color: ${defaultTheme.status.warningColorActive};
+        }
+    `,
+    danger: () => `
+        background-color: ${defaultTheme.status.errorColor};
+        color: ${defaultTheme.textColorInverted};
+        &:hover, &:focus {
+            background-color: ${defaultTheme.status.errorColorHover};
+            outline: none;
+        }
+        &:active {
+            background-color: ${defaultTheme.status.errorColorActive};
+        }
+    `,
+}
+
+
+const getBackgroundColor = (props: any, type: "normal" | "hover" = "normal"): string => {
+    if (props.secondary) {
+        return type === "hover" ? props.theme.primaryColor : "white";
+    }
+    return props.theme.primaryColor;
+}
+
+const getBorderStyles = (props: any): string => {
+    if (props.secondary) {
+        return `2px solid ${props.theme.primaryColor}`;
+    }
+    return 'none';
+}
+
+const getColor = (props: any, type: "hover" | "normal" = "normal") => {
+    if (props.secondary) {
+        return type === "hover" ? "white" : props.theme.primaryColor;
+    }
+    return props.theme.textColorOnPrimary;
+}
+interface IButton {
+    primary?: boolean;
+    secondary?: boolean;
+    info?: boolean;
+    warning?: boolean;
+    danger?: boolean;
+}
+
+export const Button = styled.button<IButton>`
+    padding: ${props => props.secondary ? "10px 24px" : "12px 24px"};
     font-size: ${typeScale.paragraph};
     border-radius: 5px;
     min-width: 100px;
     cursor: pointer;
+    box-sizing: border-box;
+    border: ${props => getBorderStyles(props)};
+    color: ${props => getColor(props)};
+    background-color: ${props => getBackgroundColor(props)};
+    transition: background-color 0.2s linear, color 0.2s linear;
+
+    &:hover {
+        background-color: ${props => props.theme.primaryHoverColor};
+        color: ${props => getColor(props, "hover")};
+    }
+
+    &:focus {
+       outline: 0px;
+    }
+
+    &:disabled {
+        background-color: ${defaultTheme.disabled};
+        color: ${defaultTheme.testOnDisabled};
+        cursor: not-allowed;
+        border: ${props => props.secondary ? `2px solid ${defaultTheme.testOnDisabled}` : 'none'};
+    }
+
+    ${applyStyleModifiers(HEADING_MODIFIERS)}
 `;
 
-const PrimaryButton = styled(Button)`
-    background-color: ${defaultTheme.primaryColor};
-    border: none;
-    color: white;
-`;
-const InfoButton = styled(Button)`
-    background-color: ${defaultTheme.primaryColor};
-    border: none;
-    color: white;
-`;
 
-const WarningButton = styled(Button)`
-    background-color: ${defaultTheme.primaryColor};
-    border: none;
-    color: white;
+interface CProps {
+    primary?: boolean;
+    secondary?: boolean;
+    info?: boolean;
+    warning?: boolean;
+    danger?: boolean;
+}
+export const CButton = styled.button<CProps>`
+    color: ${props => props.primary ? "whilte" : "blue"};
+    ${applyStyleModifiers(HEADING_MODIFIERS)}
 `;
-
-const DangerButton = styled(Button)`
-    background-color: ${defaultTheme.primaryColor};
-    border: none;
-    color: white;
-`;
-
-export default PrimaryButton;
-export { InfoButton, WarningButton, DangerButton };
